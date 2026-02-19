6112555f6cea823199f6885abf0b26690ace7b1e
Tom Parker <parker.tom.f@gmail.com>
2026-02-18 23:01:35 -0500

    Fix incorrect name of yapf variable; switch to pyproject.toml to be more modern; format inference.py (failed before due incorrect variable name); add build status to readme

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+![Build Status](https://github.com/parkertomf/llm-reasoning-evaluator/actions/workflows/lint.yml/badge.svg)
+
 # llm-reasoning-evaluator
 Automated Evaluation of LLM Reasoning Using Structured Rubrics
--- src/inference.py
+++ src/inference.py
@@ -1,21 +1,24 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 
-
 # Dataset and model setup
 dataset = list(load_dataset("gsm8k", "main")["test"])  # main as opposed to Socratic
 questions = [qa["question"] for qa in dataset[:5]]
-correct_answers = [qa["answer"][qa["answer"].rindex("####") + 4 :] for qa in dataset[:5]]
+correct_answers = [qa["answer"][qa["answer"].rindex("####") + 4:] for qa in dataset[:5]]
 model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
 
 # Prompt formatting for model
-messages = [{"role": "system", "content": "For each question, respond only with your numerical answer."}]
+messages = [{
+    "role": "system",
+    "content": "For each question, respond only with your numerical answer."
+}]
 formatted_messages = []
 for q in questions:
     messages.append({"role": "user", "content": q})
-    formatted_messages.append(tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True))
+    formatted_messages.append(
+        tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True))
     messages.pop()
 model_inputs = tokenizer(formatted_messages, padding=True, return_tensors="pt").to(model.device)
 
@@ -27,8 +30,7 @@ generated_ids = model.generate(
     # They are set simply to suppress an info log.
     temperature=1.0,
     top_k=50,
-    top_p=1.0
-)
+    top_p=1.0)
 
 # Remove the prompt tokens from the output so only the model’s new answer remains.
 generated_ids = [
@@ -39,4 +41,6 @@ generated_ids = [
 # Handle ouput
 model_responses = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
 for i, q in enumerate(questions):
-    print(f"Question: {q}\nCorrect Response: {correct_answers[i]}\nModel Response: {model_responses[i]}\n\n")
+    print(
+        f"Question: {q}\nCorrect Response: {correct_answers[i]}\nModel Response: {model_responses[i]}\n\n"
+    )
