c20d57b81ca3f0ff7a081d5125a9bcd7bc2c45c5
Tom Parker <parker.tom.f@gmail.com>
2026-02-19 00:31:03 -0500

    Added accuracy computing and outputting a summary. Set max_new_tokens to 16 (small) since we are only asking for a numerical output

--- src/inference.py
+++ src/inference.py
@@ -1,10 +1,12 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from datasets import load_dataset
 
+eval_count = 100
+
 # Dataset and model setup
 dataset = list(load_dataset("gsm8k", "main")["test"])  # main as opposed to Socratic
-questions = [qa["question"] for qa in dataset[:5]]
-correct_answers = [qa["answer"][qa["answer"].rindex("####") + 4:] for qa in dataset[:5]]
+questions = [qa["question"] for qa in dataset[:eval_count]]
+correct_answers = [qa["answer"][qa["answer"].rindex("####") + 5:] for qa in dataset[:eval_count]]
 model_name = "Qwen/Qwen2.5-1.5B-Instruct"
 model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
 tokenizer = AutoTokenizer.from_pretrained(model_name, padding_side="left")
@@ -25,6 +27,7 @@ model_inputs = tokenizer(formatted_messages, padding=True, return_tensors="pt").
 # Actually generate
 generated_ids = model.generate(
     **model_inputs,
+    max_new_tokens=16,
     do_sample=False,  # Consistent output. Greedy token generation
     # The following 3 params are default values and are irrelevant parameters when used with do_sample=False
     # They are set simply to suppress an info log.
@@ -38,9 +41,30 @@ generated_ids = [
     for input_ids, output_ids in zip(model_inputs.input_ids, generated_ids)
 ]
 
-# Handle ouput
 model_responses = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-for i, q in enumerate(questions):
-    print(
-        f"Question: {q}\nCorrect Response: {correct_answers[i]}\nModel Response: {model_responses[i]}\n\n"
-    )
+
+# Handle output
+correct, incorrect, extraction_failures = 0, 0, 0
+for i in range(eval_count):
+    model_res = model_responses[i]
+    if not model_res.isdigit():
+        extraction_failures += 1
+    elif model_res == correct_answers[i]:
+        correct += 1
+    else:
+        incorrect += 1
+
+# for i, q in enumerate(questions):
+#     print(
+#         f"Question: {q}\nCorrect Response: {correct_answers[i]}\nModel Response: {model_responses[i]}\n\n"
+#     )
+
+print(
+    f"Model: {model_name}\n"
+    f"Problems Tested: {eval_count}\n"
+    f"Correct: {correct}\n"
+    f"Incorrect: {incorrect}\n"
+    f"Extraction Failures: {extraction_failures}\n"
+    f"Accuracy: {(correct / eval_count * 100):.1f}%\n"
+    f"Accuracy on Extraction Success: {(correct / (eval_count - extraction_failures) * 100):.1f}%\n"
+    f"Format Compliance: {((eval_count - extraction_failures) / eval_count * 100):.1f}%")
