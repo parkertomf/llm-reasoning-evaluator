05a125c6bd7ab843a370c110fc1dbe82d13558b9
Tom Parker <parker.tom.f@gmail.com>
2026-02-25 15:58:01 -0500

    Resolve issue of running out of memory for large number of evals

--- src/evaluator/models.py
+++ src/evaluator/models.py
@@ -2,6 +2,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
 class ModelWrapper:
+
     def __init__(self, name: str):
         self.name = name
         self.model = AutoModelForCausalLM.from_pretrained(name, torch_dtype="auto",
--- src/evaluator/utils.py
+++ src/evaluator/utils.py
@@ -3,13 +3,13 @@ from evaluator.datasets import GSM8KDataset
 
 
 def sort_output(
-    model_responses: List[str],
+    decoded_responses: List[str],
     dataset: GSM8KDataset,
 ) -> Tuple[int, int, int]:
     """Get the counts of correct answers, incorrect answers, and extraction failures from a model's response."""
     correct, incorrect, extract_fails = 0, 0, 0
-    for i in range(len(model_responses)):
-        model_res = model_responses[i]
+    for i in range(len(decoded_responses)):
+        model_res = decoded_responses[i]
         if not dataset.is_valid_answer(model_res):
             extract_fails += 1
         elif model_res == dataset.correct_answers[i]:
@@ -26,11 +26,12 @@ def print_statistics(
     model_name: str,
 ) -> None:
     eval_count = correct + incorrect + extract_fails
-    print(f"\nModel: {model_name}\n"
-          f"Problems Tested: {eval_count}\n"
-          f"Correct: {correct}\n"
-          f"Incorrect: {incorrect}\n"
-          f"Extraction Failures: {extract_fails}\n"
-          f"Accuracy: {(correct / eval_count * 100):.1f}%\n"
-          f"Accuracy on Extraction Success: {(correct / (eval_count - extract_fails) * 100):.1f}%\n"
-          f"Format Compliance: {((eval_count - extract_fails) / eval_count * 100):.1f}%\n")
+    print(
+        f"\nModel: {model_name}\n"
+        f"Problems Tested: {eval_count}\n"
+        f"Correct: {correct}\n"
+        f"Incorrect: {incorrect}\n"
+        f"Extraction Failures: {extract_fails}\n"
+        f"Accuracy: {(correct / eval_count * 100):.1f}%\n"
+        f"Extraction Success Rate: {((eval_count - extract_fails) / eval_count * 100):.1f}%\n"
+        f"Accuracy on Extraction Success: {(correct / (eval_count - extract_fails) * 100):.1f}%\n")
